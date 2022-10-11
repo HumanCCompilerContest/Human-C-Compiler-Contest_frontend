6e16bfb39c43c317bd1c32ea32b91dacda7baca4
karintou8710 <karintou8710@gmail.com>
2022-10-11 22:29:26 +0900

    [add] Link to header button

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,10 +3,13 @@ import LoginIcon from '@mui/icons-material/Login'
 import AppBar from '@mui/material/AppBar'
 import IconButton from '@mui/material/IconButton'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import { useTheme } from '@mui/material/styles'
 import Image from 'next/image'
 import * as React from 'react'
+import Link from 'next/link'
+import MuiLink from '@mui/material/Link'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 import ButtonWithIcon from '@/components/molecules/ButtonWithIcon'
 
@@ -20,23 +23,52 @@ const Header = () => {
       }}
     >
       <Toolbar>
-        <IconButton
-          size='large'
-          aria-label='menu'
-          sx={{ margin: '0 0.5rem 0 1rem' }}
-          disabled
-        >
-          <Image src='/HCCC_logo.png' layout='fill' />
-        </IconButton>
+        <Link href='/' passHref>
+          <MuiLink
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <IconButton
+              size='large'
+              aria-label='menu'
+              sx={{ margin: '0 0.5rem 0 1rem' }}
+              disabled
+            >
+              <Image src='/HCCC_logo.png' layout='fill' />
+            </IconButton>
+            <Typography variant='h6' component='span' sx={{ color: 'white' }}>
+              HCCC
+            </Typography>
+          </MuiLink>
+        </Link>
 
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          HCCC
-        </Typography>
-        <ButtonWithIcon buttonLabel='Login' iconReactNode={<LoginIcon />} />
-        <ButtonWithIcon
-          buttonLabel='Register'
-          iconReactNode={<HowToRegIcon />}
-        />
+        <Box sx={{ flexGrow: 1 }} />
+
+        <Link href='/login' passHref>
+          <MuiLink
+            sx={{
+              color: 'white',
+            }}
+          >
+            <ButtonWithIcon buttonLabel='Login' iconReactNode={<LoginIcon />} />
+          </MuiLink>
+        </Link>
+
+        <Link href='/register' passHref>
+          <MuiLink
+            sx={{
+              color: 'white',
+            }}
+          >
+            <ButtonWithIcon
+              buttonLabel='Register'
+              iconReactNode={<HowToRegIcon />}
+            />
+          </MuiLink>
+        </Link>
       </Toolbar>
     </AppBar>
   )
