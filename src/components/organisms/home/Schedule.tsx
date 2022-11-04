c4c50ca92fbbcd81a9dde3477af5cc08e7357f78
karintou8710 <karintou8170@gmail.com>
2022-11-04 16:59:52 +0900

    fix schedule style

--- src/components/organisms/home/Schedule.tsx
+++ src/components/organisms/home/Schedule.tsx
@@ -1,24 +1,37 @@
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 
 const Schedule = () => {
   return (
-    <Box>
-      <Typography variant='h3'>Schedule</Typography>
-      <Typography>2022年11月19日(土)</Typography>
-      <Typography>
-        SECCONCON(SECCON Contests)は SECCON Contest of Contest
-        に応募された競技やコンテストの企画案・設計案を，実際に実施するイベントです．
-      </Typography>
-      <Typography>
-        <Link
-          href='https://www.seccon.jp/2022/seccon_contest/secconcon.html'
-          target='_blank'
-        >
-          SECCONCON 2022
-        </Link>
-      </Typography>
+    <Box sx={{ margin: '4rem 0' }}>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <CalendarMonthIcon
+          sx={{ width: '60px', height: '60px', marginRight: '1rem' }}
+        />
+        <Typography variant='h3'>Schedule</Typography>
+      </Box>
+
+      <Box sx={{ width: '600px', margin: '3rem auto' }}>
+        <Typography variant='h4' align='center'>
+          2022年11月19日(土) 13:10 〜
+        </Typography>
+        <Typography variant='h6' sx={{ margin: '2rem 0 1rem' }}>
+          SECCONCON(SECCON Contests)は SECCON Contest of Contest
+          に応募された競技やコンテストの企画案・設計案を，実際に実施するイベントです．
+        </Typography>
+        <Typography align='center'>
+          <Link
+            href='https://www.seccon.jp/2022/seccon_contest/secconcon.html'
+            target='_blank'
+          >
+            SECCONCON 2022
+          </Link>
+        </Typography>
+      </Box>
     </Box>
   )
 }
