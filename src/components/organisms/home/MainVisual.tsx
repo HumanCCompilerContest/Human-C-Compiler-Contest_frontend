7ff5d89ab3a713780e5ce4de748ef9ee8e2105e7
karintou8710 <karintou8170@gmail.com>
2022-11-07 01:22:39 +0900

    refactor MainVisual breakpoint

--- src/components/organisms/home/MainVisual.tsx
+++ src/components/organisms/home/MainVisual.tsx
@@ -1,11 +1,9 @@
-import { useMediaQuery } from '@mui/material'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { useTheme } from '@mui/material/styles'
 import Image from 'next/image'
 
 const MainVisual = () => {
-  const match = useMediaQuery('(min-width:577px)')
   const theme = useTheme()
   return (
     <Box
@@ -19,31 +17,34 @@ const MainVisual = () => {
     >
       <Box
         sx={{
-          maxWidth: match ? '1000px' : '90%',
+          maxWidth: { xs: '90%', sm: '1000px' },
           bgcolor: theme.palette.primary.main,
           color: 'white',
-          padding: match ? '4rem' : '2rem',
+          padding: { xs: '2rem', sm: '4rem' },
           opacity: 0.8,
           display: 'flex',
           borderRadius: '1rem',
         }}
       >
-        {match && <Image src='/HCCC_logo.png' width='200px' height='200px' />}
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Image src='/HCCC_logo.png' width='200px' height='200px' />
+        </Box>
 
         <Box sx={{ padding: '1rem' }}>
           <Typography
             variant='h4'
             component='div'
             sx={{
-              width: match ? '600px' : undefined,
+              width: { xs: undefined, sm: '600px' },
               fontWeight: '800',
               paddingBottom: '3rem',
             }}
           >
             あなたも
-            <span
-              style={{
-                display: match ? undefined : 'block',
+            <Box
+              component='span'
+              sx={{
+                display: { xs: 'block', sm: 'inline' },
                 fontSize: '2.5rem',
                 padding: '0 0.5rem',
                 fontWeight: '900',
@@ -51,7 +52,7 @@ const MainVisual = () => {
               }}
             >
               Cコンパイラ
-            </span>
+            </Box>
             になろう
           </Typography>
           <Typography
