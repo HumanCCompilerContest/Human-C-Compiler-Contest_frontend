e9d0bf382eb277496c4f892e62fa58b2600329ab
karintou8710 <karintou8170@gmail.com>
2022-11-07 18:37:08 +0900

    refactor QandA

--- src/components/organisms/home/QandA.tsx
+++ src/components/organisms/home/QandA.tsx
@@ -1,98 +1,108 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import HelpIcon from '@mui/icons-material/Help'
-import Accordion from '@mui/material/Accordion'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import AccordionSummary from '@mui/material/AccordionSummary'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { useTheme } from '@mui/material/styles'
+import {
+  Box,
+  Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from '@mui/material'
+import { styled } from '@mui/material/styles'
+import type { SxProps, Theme } from '@mui/material/styles'
+import { FC } from 'react'
 
-const QandA = () => {
-  const theme = useTheme()
+type QandAProps = {
+  sx?: SxProps<Theme>
+}
+
+const QandA: FC<QandAProps> = ({ sx }) => {
   return (
-    <div>
-      <Box
+    <Box sx={sx}>
+      <Typography
+        variant='h3'
         sx={{
-          margin: '10rem 0 5rem',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          fontWeight: '600',
+          m: '10rem 0 5rem',
+          '&::before, &::after': {
+            content: '""',
+            display: 'block',
+            maxWidth: '5rem',
+            borderTop: 'solid 2px',
+            flexGrow: 1,
+          },
+          '&::before': {
+            mr: '2rem',
+          },
+          '&::after': {
+            ml: '2rem',
+          },
         }}
       >
-        <Typography variant='h3' sx={{ fontWeight: '600' }}>
-          - Q&A -
-        </Typography>
-      </Box>
+        Q&A
+      </Typography>
 
       <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          sx={{ padding: '1rem', backgroundColor: 'rgba(0, 0, 0, .03)' }}
-        >
+        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
           <HelpIcon color='secondary' sx={{ mr: '1rem' }} />
-          <Typography>
-            競技で使用できるCのバージョン, アーキテクチャを教えてください
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ p: '2rem 1rem' }}>
-          <Typography>
-            C99, X86_64を採用しています。ABIの仕様はSystem Vの仕様に従います。
-          </Typography>
-        </AccordionDetails>
+          競技で使用できるCのバージョン, アーキテクチャを教えてください
+        </StyledAccordionSummary>
+        <StyledAccordionDetails>
+          C99, X86_64を採用しています。ABIの仕様はSystem Vの仕様に従います。
+        </StyledAccordionDetails>
       </Accordion>
+
       <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          sx={{ padding: '1rem', backgroundColor: 'rgba(0, 0, 0, .03)' }}
-        >
+        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
           <HelpIcon color='secondary' sx={{ mr: '1rem' }} />
-          <Typography>
-            初心者でコンパイラの知識がありませんが、参加できますか？
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ p: '2rem 1rem' }}>
-          <Typography>
-            大歓迎です。裏番組として入門編を流す予定ですので自分のレベル感に合わせて解説を聞いたり競技に戻ったりして頂いて構いません．
-            開催一週間前までにはチュートリアルも用意する予定なので、そちらで予習も可能です。
-            低レイヤーに踏み出す第一歩としてご活用ください。
-          </Typography>
-        </AccordionDetails>
+          初心者でコンパイラの知識がありませんが、参加できますか？
+        </StyledAccordionSummary>
+        <StyledAccordionDetails>
+          大歓迎です。裏番組として入門編を流す予定ですので自分のレベル感に合わせて解説を聞いたり競技に戻ったりして頂いて構いません．
+          開催一週間前までにはチュートリアルも用意する予定なので、そちらで予習も可能です。
+          低レイヤーに踏み出す第一歩としてご活用ください。
+        </StyledAccordionDetails>
       </Accordion>
+
       <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          sx={{ padding: '1rem', backgroundColor: 'rgba(0, 0, 0, .03)' }}
-        >
+        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
           <HelpIcon color='secondary' sx={{ mr: '1rem' }} />
-          <Typography>AT&T記法とIntel記法のどちらも使えますか？</Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ p: '2rem 1rem' }}>
-          <Typography>使えます。お好きな方で記述してください。</Typography>
-        </AccordionDetails>
+          AT&T記法とIntel記法のどちらも使えますか?
+        </StyledAccordionSummary>
+        <StyledAccordionDetails>
+          使えます。お好きな方で記述してください。
+        </StyledAccordionDetails>
       </Accordion>
+
       <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          sx={{ padding: '1rem', backgroundColor: 'rgba(0, 0, 0, .03)' }}
-        >
+        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
           <HelpIcon color='secondary' sx={{ mr: '1rem' }} />
-          <Typography>詳細のルールはどこから確認できますか？</Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ p: '2rem 1rem' }}>
-          <Typography>
-            <a
-              href='https://github.com/Alignof/Human_C_Compiler_Contest'
-              target='_blank'
-              rel='noreferrer'
-            >
-              こちらのリンク
-            </a>
-            からご確認ください。
-          </Typography>
-        </AccordionDetails>
+          詳細のルールはどこから確認できますか？
+        </StyledAccordionSummary>
+        <StyledAccordionDetails>
+          <a
+            href='https://github.com/Alignof/Human_C_Compiler_Contest'
+            target='_blank'
+            rel='noreferrer'
+          >
+            こちらのリンク
+          </a>
+          からご確認ください。
+        </StyledAccordionDetails>
       </Accordion>
-    </div>
+    </Box>
   )
 }
+
+const StyledAccordionSummary = styled(AccordionSummary)({
+  padding: '1rem',
+  backgroundColor: 'rgba(0, 0, 0, .03)',
+})
+
+const StyledAccordionDetails = styled(AccordionDetails)({
+  padding: '2rem 1rem',
+})
 
 export default QandA
