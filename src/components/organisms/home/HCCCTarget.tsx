9deb378b81cd7352ed4fc431c98e81a7979db268
karintou8710 <karintou8170@gmail.com>
2022-11-10 22:34:52 +0900

    fix margin style

--- src/components/organisms/home/HCCCTarget.tsx
+++ src/components/organisms/home/HCCCTarget.tsx
@@ -10,7 +10,7 @@ type HCCCTargetProps = {
 const HCCCTarget: FC<HCCCTargetProps> = ({ sx }) => {
   return (
     <DiscriptionCard title='競技の目的' sx={sx}>
-      <p style={{ m: 0 }}>
+      <p style={{ margin: 0 }}>
         この競技の目的は競技を通じてアセンブリやC言語の仕様，ABIと仲良くなることです．
       </p>
       <p>
