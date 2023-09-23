571b050b9763616b6d3f952d3df7958826e239ed
karintou8710 <karintou8710@gmail.com>
2023-09-23 14:59:42 +0900

    [add] clock logic

--- src/features/utils.ts
+++ src/features/utils.ts
@@ -15,3 +15,25 @@ export const formatDate = (format: string, date: Date) => {
     .replace('%M', dateObj.minutes)
     .replace('%S', dateObj.seconds)
 }
+
+export const formatMillSeconds = (format: string, millSeconds: number) => {
+  const seconds = Math.ceil(millSeconds / 1000)
+  const h = Math.floor(seconds / (60 * 60))
+  const m = Math.floor((seconds - h * 60 * 60) / 60)
+  const s = seconds - (h * 60 * 60 + m * 60)
+
+  const timeObj = {
+    hours: String(h).padStart(2, '0'),
+    minutes: String(m).padStart(2, '0'),
+    seconds: String(s).padStart(2, '0'),
+  }
+  return format
+    .replace('%H', timeObj.hours)
+    .replace('%M', timeObj.minutes)
+    .replace('%S', timeObj.seconds)
+}
+
+/* 秒単位で日付を比較する */
+export const equalDateTime = (d1: Date, d2: Date) => {
+  return Math.floor(d1.getTime() / 1000) === Math.floor(d2.getTime() / 1000)
+}
