5fb340f3c807ec6ec06e948fc292509b608519ea
karintou8710 <karintou8710@gmail.com>
2023-09-24 21:16:06 +0900

    [add] show ranking page is freezed

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,17 +1,30 @@
 import StarIcon from '@mui/icons-material/Star'
-import { Typography } from '@mui/material'
+import { Alert, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Error from 'next/error'
 import Head from 'next/head'
 
+import { useEffect, useState } from 'react'
 import Loading from '@/components/atoms/Loading'
 import TextWithIcon from '@/components/atoms/TextWithIcon'
 import RankingTable from '@/components/molecules/RankingTable'
 import BasicLayout from '@/components/templates/BasicLayout'
 import { useRanking } from '@/features/api'
 
+const CONTEST_END_DATE = new Date(process.env.NEXT_PUBLIC_CONTEST_END || '')
+
 const Ranking: NextPage = () => {
   const { rankingResponse, isError } = useRanking()
+  const [now, setNow] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow(new Date())
+    }, 200)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
 
   if (isError) {
     return <Error statusCode={isError.status} title={isError.message} />
@@ -41,6 +54,13 @@ const Ranking: NextPage = () => {
             Ranking
           </Typography>
         </TextWithIcon>
+
+        {now > CONTEST_END_DATE && (
+          <Alert severity='info' sx={{ m: '2rem 0' }}>
+            コンテストは終了しました。
+          </Alert>
+        )}
+
         <RankingTable rankingList={rankingList} />
       </BasicLayout>
     </>
